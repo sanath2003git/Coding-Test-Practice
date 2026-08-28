97f70626c6c009f149b46443a5f5749e42dee7f7
C S Sanath Sreekumar <165697113+sanath2003git@users.noreply.github.com>
2026-08-28 11:25:31 +0530

    Update Solution.py

--- 01_Longest_Valid_Parentheses/Solution.py
+++ 01_Longest_Valid_Parentheses/Solution.py
@@ -1,19 +1,18 @@
 def longest_valid_parentheses(s):
-    stack = []
-    stack.append(-1)
+    stack = [-1]
     max_length = 0
     for i in range(len(s)):
-        if(s[i] == "("):
+        if s[i] == "(":
             stack.append(i)
         else:
             stack.pop()
             if not stack:
                 stack.append(i)
             else:
-                length = i - stack[-1]
+                length = i - stack[-1]    
                 max_length = max(max_length, length)
     return max_length
 
-s = ")()())"
+s = ")(()())"
 result = longest_valid_parentheses(s)
 print(result)
