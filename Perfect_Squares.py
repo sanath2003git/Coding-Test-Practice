28fb5a07cd2748d684c8aeb6ec9b9df6af220cf5
C S Sanath Sreekumar <165697113+sanath2003git@users.noreply.github.com>
2026-08-28 16:10:35 +0530

    Create Perfect_Squares.py

---
+++ Perfect_Squares.py
@@ -0,0 +1,21 @@
+def minimum_squares(n):
+
+    dp = [0] * (n + 1)
+
+    for i in range(1 ,n + 1):
+
+        dp[i] = i
+        j = 1
+
+        while j * j <= i:
+
+            dp[i] = min(dp[i], 1 + dp[i - j * j])
+            j += 1
+
+    return dp[n]
+
+n = 12
+
+result = minimum_squares(n)
+
+print(result)
